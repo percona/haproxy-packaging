327a51dfe19f31cc1feb83ca1f2f8724193ff2a0
Evgeniy Patlan <evgeniy.patlan@percona.com>
2021-02-24 15:21:13 +0200

    DISTMYSQL-85 update haproxy

--- haproxy-builder.sh
+++ haproxy-builder.sh
@@ -406,12 +406,12 @@ INSTALL=0
 RPM_RELEASE=1
 DEB_RELEASE=1
 REVISION=0
-BRANCH="v2.3.0"
+BRANCH="v2.3.2"
 REPO="http://git.haproxy.org/git/haproxy-2.3.git/"
 PRODUCT=percona-haproxy
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='2.3.0'
+VERSION='2.3.2'
 RELEASE='1'
 PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
 
