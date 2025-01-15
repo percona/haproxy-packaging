45771837faabd59fc60f0774b630abcfcd1fd4db
Surabhi Bhat <surabhi.bhat@percona.com>
2025-01-15 12:01:40 +0530

    PKG-415 Build HAProxy on Amazon Linux 2023

--- haproxy-builder.sh
+++ haproxy-builder.sh
@@ -138,6 +138,11 @@ get_system(){
         ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
         OS_NAME="el$RHEL"
         OS="rpm"
+    elif [ -f /etc/amazon-linux-release ]; then
+        RHEL=$(rpm --eval %amzn)
+        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+        OS_NAME="amzn$RHEL"
+        OS="rpm"
     else
         ARCH=$(uname -m)
         OS_NAME="$(lsb_release -sc)"
@@ -185,8 +190,7 @@ install_deps() {
         fi
         yum -y install wget
         yum clean all
-        RHEL=$(rpm --eval %rhel)
-        INSTALL_LIST="git wget rpm-build gcc make tar pcre2-devel zlib-devel openssl-devel systemd-devel readline-devel"
+        INSTALL_LIST="git wget rpm-build gcc make tar pcre2-devel zlib-devel openssl-devel systemd systemd-devel readline-devel"
         yum -y install ${INSTALL_LIST}
 	yum -y install lua-devel 
         install_lua
@@ -317,7 +321,6 @@ build_rpm(){
     cd rb/SRPMS/
     #
     cd $WORKDIR
-    RHEL=$(rpm --eval %rhel)
     ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
     rpmbuild --define "_topdir ${WORKDIR}/rb" --define "dist .$OS_NAME" --define "version ${VERSION}" --rebuild rb/SRPMS/$SRC_RPM
 
